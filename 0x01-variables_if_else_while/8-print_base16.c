07b3e3fb359d44dd4a1a64a008d41c0c4b9358c6
Ali Mohamed <amohamd31@gmail.com>
2023-02-17 14:35:50 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return:always return 0
+ *
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+	for (num = 0; num < 6; num++)
+	{
+		putchar('a' + num);
+	}
+	putchar('\n');
+	return (0);
+}
